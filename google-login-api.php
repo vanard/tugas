c8788e2e711e3dadc0df1f7d117fdb2c22fff654
Vian RD <vianixa34@gmail.com>
2018-11-09 20:45:25 +0700

    Update google-login-api.php

--- google-login-api.php
+++ google-login-api.php
@@ -44,6 +44,7 @@ class GoogleLoginApi
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 		$data = json_encode(curl_exec($ch), true);
+		$data = json_decode($data);
 
 		$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);		
 		if($http_code != 200) 
