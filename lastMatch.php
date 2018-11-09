01ecf8ee22b5f1540701cd20980319924f77c84f
Vian RD <vianixa34@gmail.com>
2018-11-09 21:59:59 +0700

    Update lastMatch.php

--- lastMatch.php
+++ lastMatch.php
@@ -4,6 +4,7 @@ require_once('google-login-api.php');
 
 if(isset($_GET['id'])){
     $id = $_GET['id'];
+    
 try {
     $gapi = new GoogleLoginApi();
     
@@ -16,6 +17,7 @@ catch(Exception $e) {
     echo $e->getMessage();
     exit();
 }
+    
 }else{
     echo "belom set parameter mas";
 }
